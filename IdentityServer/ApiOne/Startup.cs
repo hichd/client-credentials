4f0af18a55903431b769c6b9f0acb3b3e07a4dfd
Hicham Daou <hicham_daou@hotmail.com>
2021-02-21 02:17:11 -0500

    ApiOne setup
    - JWT-Bearer authentication
    - endpoint returns secret message

---
+++ IdentityServer/ApiOne/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiOne
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication()
+                .AddJwtBearer("Bearer", config =>
+                {
+                    config.Authority = "https://localhost:44385/"; // identity server url : tell api where to pass access tokens to validate
+
+                    config.Audience = "ApiOne"; // tell identity server who is validating the token
+                });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
